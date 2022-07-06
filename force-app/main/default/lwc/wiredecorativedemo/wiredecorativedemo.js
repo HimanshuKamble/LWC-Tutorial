102ea6958dc9707140650321eb329b767ef1545e
Himanshu Kamble <himanshusfdc2019@gmail.com>
2022-07-06 14:37:56 +0530

    newwirecomponent

--- force-app/main/default/lwc/wiredecorativedemo/wiredecorativedemo.js
+++ force-app/main/default/lwc/wiredecorativedemo/wiredecorativedemo.js
@@ -1,22 +1,21 @@
 import { LightningElement,track,wire } from 'lwc';
-import getcontactlist from '@salesforce/apex/c/wiredecoraticedemocomponentclassfromvs.getaccountlist' ;
+import getcontactlist from '@salesforce/apex/wiredecoraticedemocomponentclass.getcontactlist' ;
 
 const columns1=[
-    {label:'Name' , field : "Name" },
-    {label:'Player Record Id' , field : "ID" },
-    {label:'Test Columns' , field : "Last Name" },
+    {label:'Contact Record Id' , fieldName : "Id" },
+    {label:'First Name' , fieldName : "Name" },
 ];
 export default class Wiredecorativedemo extends LightningElement {
 
     @track columns=columns1; 
-    @track data = [];
+    @track datas = [];
 
     @wire(getcontactlist)
     wireconlistclass({data , error}){
         if(data){
-            this.data=data;
+            this.datas=data;
         } else if (error){
             console.log("Error Has been Occured");
-        }
-    }
+        } 
+    } 
 }
